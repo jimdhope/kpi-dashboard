b4415721e349cf040494e449ad0c49c75c1fdcf5
Jim Hope <jimdhope@gmail.com>
2026-04-07 09:03:29 +0100

    Fix: Use bind mount for postgres data, add database creation to init script

--- docker/postgres-init/01-init.sql
+++ docker/postgres-init/01-init.sql
@@ -1,1 +1,10 @@
+-- Set password for postgres user
 ALTER USER postgres WITH PASSWORD 'postgres';
+
+-- Create the database if it doesn't exist
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'kpi_quest_v3') THEN
+      CREATE DATABASE kpi_quest_v3;
+   END IF;
+END $$;
